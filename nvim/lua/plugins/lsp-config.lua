32cff4da60846a97c2c61138d23dc9c9c2923017
Pandolf <pandolf99@gmail.com>
2025-11-09 18:18:01 -0500

    remove mason

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,24 @@
+-- Add LSPs here
+-- Make sure they are generally available in the path
+return {
+	"neovim/nvim-lspconfig",
+	-- This is a bit different than other plugins.
+	-- We call this so that lsps are configured correctly
+	-- on nvim startup, and there is no "require" for lsp
+	config = function()
+		vim.diagnostic.config({
+			virtual_text = true,
+		})
+		vim.lsp.config("gopls", {
+			settings = {
+				gopls = {
+					buildFlags = {
+						"-tags=unit,integration,payloads",
+					},
+				},
+			},
+		})
+		vim.lsp.enable("gopls")
+		vim.lsp.enable("pyright")
+	end,
+}
