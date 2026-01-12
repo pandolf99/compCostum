c1b458727f4d0bd744f71587bec6eb4e021210c2
pandolf <pandolf99@gmail.com>
2026-01-12 17:48:35 -0600

    add octo and typescript lsp

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-			ensure_installed = { "python", "bash", "go", "javascript" },
+			ensure_installed = { "python", "bash", "go", "javascript", "typescript"},
 			sync_install = false,
 			auto_install = false,
 			highlight = { enable = true },
